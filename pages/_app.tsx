53f98af3168b63c00c689697a66ce3c56a3da72e
sebastiantheprogrammer <sebastianingram121510@gmail.com>
2026-06-23 19:29:18 -0400

    Apply script fonts and refine admin spacing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pinyon+Script&family=Story+Script&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
